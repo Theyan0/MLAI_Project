8c9c7f9a26119e9db51b5e237c2425a134839d59
Aidan <aidang.22@ichat.sp.edu.sg>
2025-01-13 00:22:25 +0800

    edit

--- app.py
+++ app.py
@@ -3,44 +3,44 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 
-# Function to preprocess the image and make predictions
 def import_and_predict(image_data, model):
-    size = (150, 150)  # Resize to match the model's expected input size
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    image = image.convert('RGB')
-    image = np.asarray(image)
-    image = (image.astype(np.float32) / 255.0)  # Normalize pixel values
+    
+        size = (150,150)    
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
+        image = image.convert('RGB')
+        image = np.asarray(image)
+        image = (image.astype(np.float32) / 255.0)
 
-    img_reshape = image[np.newaxis, ...]  # Add batch dimension
+        img_reshape = image[np.newaxis,...]
 
-    prediction = model.predict(img_reshape)  # Predict the class probabilities
-    return prediction
+        prediction = model.predict(img_reshape)
+        
+        return prediction
 
-# Load the trained model
-model = tf.keras.models.load_model('C:/Users/sunda/OneDrive/Documents/ML&AI LABS/MLAI_Project/Models/model01_theyan.h5')
+model = tf.keras.models.load_model('C:/Users/Aidan/Important/MLAI_Project/MLAI_Project/Models/model01_theyan.h5') #loading a trained model
 
-# Streamlit app title and description
 st.write("""
-         # Bao, Random, Uni Sushi Classification
+         # Rock-Paper-Scissor Hand Sign Prediction
          """
          )
-st.write("This is a simple image classification web app to predict whether the image is Bao, Random, or Uni Sushi.")
 
-# File uploader for image input
+st.write("This is a simple image classification web app to predict rock-paper-scissor hand sign")
+
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
-
+#
 if file is None:
-    st.text("You haven't uploaded an image file.")
+    st.text("You haven't uploaded an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    
-    # Make prediction
     prediction = import_and_predict(image, model)
-    class_names = ['Bao', 'Random', 'Uni Sushi']  # Class labels corresponding to the model's output
-
-    predicted_class = class_names[np.argmax(prediction)]
-    st.write(f"The image is classified as: **{predicted_class}**")
     
-    st.text("Probability (0: Bao, 1: Random, 2: Uni Sushi)")
+    if np.argmax(prediction) == 0:
+        st.write("It is bao")
+    elif np.argmax(prediction) == 1:
+        st.write("It is neither")
+    else:
+        st.write("It is uni sushi")
+    
+    st.text("Probability (0: bao, 1: neither, 2: uni sushi)")
     st.write(prediction)
